2969e154201875f082d0ad33fb0e7748191cf152
Galmaeki <kjh09800@gmail.com>
2023-02-28 18:27:16 +0900

    Initial commit

---
+++ src/main/java/com/example/s3testproject/S3testprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3testprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3testprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s3testproject/S3testprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s3testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3testprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
